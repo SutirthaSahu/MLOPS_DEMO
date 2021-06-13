49c3d4e2c40a92f722cd231ec7eeb369ce299574
SutirthaSahu <sutirthasahu@gmail.com>
2021-06-13 20:48:30 +0530

    added test cases

--- app.py
+++ app.py
@@ -39,23 +39,26 @@ def api_response(request):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-
     if request.method == "POST":
         try:
             if request.form:
-                data = dict(request.form).values()
-                data = [list(map(float, data))]
-                response = predict(data)
+                # data = dict(request.form).values()
+                # data = [list(map(float, data))]
+                # response = predict(data)
+                data_req = dict(request.form)
+                response = prediction.form_response(data_req)
                 return render_template("index.html",response=response)
 
             elif request.json:
-                response = api_response(request)
+                # response = api_response(request)
+                response = prediction.api_response(request.json)
                 return jsonify(response)
 
         except Exception as e:
             print(e)
-            error = {"error": "something went wrong!! Try again"}
-            return render_template("404.html", error=error)
+            # error = {"error": "something went wrong!! Try again"}
+            error = {"error": e}
+            return render_template("404.html", error=e)
     else:
         return render_template("index.html")
 
